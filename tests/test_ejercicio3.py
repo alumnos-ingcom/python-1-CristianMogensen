f02f4f2282d29cc6f8b0befd68ea94fc6f4bb681
CristianMogensen <cristian.g.mog@gmail.com>
2022-06-20 16:40:06 -0300

    Test ejercicio 3 corregido.

--- tests/test_ejercicio3.py
+++ tests/test_ejercicio3.py
@@ -5,45 +5,86 @@
 
 import pytest
 
-from src.Ejercicio3 import compara
+from src.ejercicio3 import compara
 
 
 """
-Test del ejercicio 3.
-Defino 3 casos de prueba en total.
+Tests del Ejercicio 3: Comparación de números
+
+Se buscan testear 3 casos de prueba para la función compara:
+
+compara(numero: int, otro_numero: int):
+1- numero > otro_numero. Resultado esperado: 1
+2- numero < otro_numero. Resultado esperado: -1
+3- numero = otro_numero. Resultado esperado: 0
 """
 
 
-def test_compara_mayor_menor():
+def test_compara_primer_numero_mayor():
     """
-    Testeo el caso en que el primero es mayor que el segundo.
+    Test función compara(numero, otro_numero)
+    Caso de prueba 1:
+    - Entrada: numero = 33
+               otro_numero = 22
+    - Resultado esperado: resultado = 1
     """
-    N1 = 8
-    N2 = 3
-    resultado = compara(N1, N2)
-    assert isinstance(resultado, int), "El resultado debe ser un número entero."
-    assert resultado > 0, "El resultado debe ser mayor que cero."
-    assert resultado == 1, "El resultado obtenido no es el esperado."
+
+    # Entrada para probar en la función:
+    numero: int = 33
+    otro_numero: int = 22
+
+    resultado: int = compara(numero, otro_numero)
+
+    # Defino el primer mensaje de error (muy largo para pycodestyle):
+    error1_msj: str = "El resultado debe ser un número entero."
+    assert (isinstance(resultado, int)), error1_msj
+
+    assert (resultado > 0), "El resultado debe ser mayor que cero."
+
+    assert (resultado == 1), "El resultado obtenido no es el esperado."
 
 
-def test_compara_mayor_menor():
+def test_compara_primer_numero_menor():
     """
-    Testeo el caso en que el primero es mayor que el segundo.
+    Test función compara(numero, otro_numero)
+    Caso de prueba 2:
+    - Entrada: numero = 11
+               otro_numero = 33
+    - Resultado esperado: resultado = -1
     """
-    N1 = 5
-    N2 = 6
-    resultado = compara(N1, N2)
-    assert isinstance(resultado, int), "El resultado debe ser un número entero."
-    assert resultado < 0, "El resultado debe ser menor que cero."
-    assert resultado == -1, "El resultado obtenido no es el esperado."
+
+    # Entrada para probar en la función:
+    numero: int = 11
+    otro_numero: int = 33
+
+    resultado: int = compara(numero, otro_numero)
+
+    # Defino el primer mensaje de error (muy largo para pycodestyle):
+    error1_msj: str = "El resultado debe ser un número entero."
+    assert (isinstance(resultado, int)), error1_msj
+
+    assert (resultado < 0), "El resultado debe ser mayor que cero."
+
+    assert (resultado == -1), "El resultado obtenido no es el esperado."
 
 
-def test_compara_iguales():
+def test_compara_numeros_iguales():
     """
-    Testeo el caso en que el primero es mayor que el segundo.
+    Test función compara(numero, otro_numero)
+    Caso de prueba 3:
+    - Entrada: numero = 22
+               otro_numero = 22
+    - Resultado esperado: resultado = 0
     """
-    N1 = 3
-    N2 = 3
-    resultado = compara(N1, N2)
-    assert isinstance(resultado, int), "El resultado debe ser un número entero."
-    assert resultado == 0, "El resultado obtenido no es el esperado."
+
+    # Entrada para probar en la función:
+    numero: int = 22
+    otro_numero: int = 22
+
+    resultado: int = compara(numero, otro_numero)
+
+    # Defino el primer mensaje de error (muy largo para pycodestyle):
+    error1_msj: str = "El resultado debe ser un número entero."
+    assert (isinstance(resultado, int)), error1_msj
+
+    assert (resultado == 0), "El resultado obtenido no es el esperado."
