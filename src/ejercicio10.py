111638aa1d7f31ec98deefc8df62e7becf239baf
CristianMogensen <cristian.g.mog@gmail.com>
2022-06-21 19:23:06 -0300

    Ejercicio 10 corregido.

--- src/ejercicio10.py
+++ src/ejercicio10.py
@@ -5,52 +5,91 @@
 
 """
 10. Palíndromo
-"""
-
-def es_par(numero):
-    """
-    Retorna True o False si es par o impar.
-    """
-    
-    EsPar = False
-    
-    if ((numero % 2) == 0):
-        EsPar = True
-    
-    return EsPar
+Escribir una función que indique con True si una palabra o frase ingresada
+se trata de un palindromo. Palíndromo, es si se lee igual de derecha a
+izquierda que de izquierda a derecha.
 
 def es_palindromo(texto):
+"""
+
+
+def es_par(numero: int) -> bool:
     """
-    Analiza el texto o palabra ingresado y retorna True o False
-    si se trata de un palíndromo o no.
+    Esta función analiza un número y retorna True o False si es par o impar.
+    PRECONDICIONES: Recibe un número entero.
+    POSCONDICIONES: Devuelve un bool (True: par, False: impar).
     """
-    EsPalindromo = True
-    elem = 0
-    
+
+    return not (numero % 2)
+
+
+def es_palindromo(texto: str) -> bool:
+    """
+    Esta función analiza si el texto o la palabra ingresada es un palíndromo
+    o no, y retorna un booleano 'True' en caso de que sí y 'False' en caso de
+    que no lo sea.
+    PRECONDICIONES: Recibe un string.
+    POSCONDICIONES: Devuelve un bool.
+    """
+
+    # Se declara y define la variable en la que se guardará el resultado final.
+    palindromo: bool = True
+
+    # Se declara y define la variable con la que se recorrerá cada caracter del
+    # texto ingresado.
+    elem: int = 0
+
     if (es_par(len(texto))):
+
         while (elem < len(texto)//2):
-            if (texto[elem] != texto[(len(texto)-1 - elem)]):
-                EsPalindromo = False
+
+            if (texto[elem] != texto[(len(texto) - 1 - elem)]):
+
+                palindromo = False
+
                 break
+
             elem += 1
+
     else:
+
         while (elem < (len(texto)//2 + 1)):
-            if (texto[elem] != texto[(len(texto)-1 - elem)]):
-                EsPalindromo = False
+
+            if (texto[elem] != texto[(len(texto) - 1 - elem)]):
+
+                palindromo = False
+
                 break
+
             elem += 1
-    
-    return EsPalindromo
+
+    return palindromo
+
 
 def principal():
     """
-    Escribir una función que indique con True si una palabra
-    o frase ingresada se trata de un palindromo. Palíndromo,
-    es si se lee igual de derecha a izquierda que de izquierda
-    a derecha.
+    Esta función es la que se encarga de la parte 'interactiva' del ejercicio
+    (La entrada, la llamada al algoritmo y la salida)
     """
-    entrada = str(input("Ingrese palabra: "))
-    print(f"La palabra {entrada} es un palíndromo? {es_palindromo(entrada)}")
+
+    # Se imprime el título del ejercicio:
+    print("\nEjercicio 10: Palíndromo\n")
+
+    # Se hace el input del usuario.
+    texto: str = input("Ingrese palabra: ")
+
+    # Se verifica si es palíndromo.
+    palindromo: bool = es_palindromo(texto)
+
+    # Se imprime el resultado final.
+    if palindromo:
+
+        print(f"La palabra {texto} es un palíndromo.")
+
+    else:
+
+        print(f"La palabra {texto} NO es un palíndromo.")
+
 
 if __name__ == "__main__":
     principal()
